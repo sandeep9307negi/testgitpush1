e7a4216a495bf93e7347f2d429450236076d124f
sandeep9307negi <negis9307@gmail.com>
2022-08-10 10:32:01 +0530

    this is my revision code push

---
+++ hitting_sir_server.py
@@ -0,0 +1,15 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.pstrh.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d={
+    "name":"Himanshu",
+    "email":"dhf243.5456@gmail.com",
+    "surname":"Nautiyal"
+}
+
+db1=client['mongodb_revisio']
+coll=db1['test']
+coll.insert_one(d)
