3b7ae474c96515007d95472ba5111ddd225ff010
sandeep9307negi <negis9307@gmail.com>
2022-08-10 10:38:08 +0530

    this is my revision code push again

--- test1.py
+++ test1.py
@@ -4,11 +4,30 @@ client = pymongo.MongoClient("mongodb+srv://sandeep:Mech0422@cluster0.pstrh.mong
 db = client.test
 print(db)
 
+
 d={
     "name":"Himanshu",
     "email":"dhf243.5456@gmail.com",
     "surname":"Nautiyal"
 }
+d={
+    "name":"Himanshu",
+    "email":"dhf243.5456@gmail.com",
+    "surname":"Nautiyal"
+}
+d={
+    "name":"Himanshu",
+    "email":"dhf243.5456@gmail.com",
+    "surname":"Nautiyal"
+}
+
+d={
+    "name":"Himanshu",
+    "email":"dhf243.5456@gmail.com",
+    "surname":"Nautiyal"
+}
+
+
 
 db1=client['mongodb_revisio']
 coll=db1['test']
